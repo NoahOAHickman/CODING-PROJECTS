6939e0dbcdbe3c61ccc202a0af8e348c70815b49
Noah <noahoahickman@gmail.com>
2021-04-18 16:11:51 -0700

    updated with project description

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,3 +1,9 @@
-console.log('Hello world');
-console.log('commit?');
-console.log('Why won\'t this work?');
+// For my version of the Mixed Messages Codecademy project, I'm going to create 
+// a bad advice generator. It's just going to dole out bad, randomly generated advice to the user. 
+// it will output a format string with 3 peices of data in it, all randomly selected from a list of words
+// I will also include a 'Name your firstborn child ${literalRandomLetters}', because why not have a 
+// legitimately randomly generated portion in there?
+
+
+
+
